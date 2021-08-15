fe72c0504774b656ce72e5c1f918e19e4d18eb47
Gabriel Antonio <oliveiragabr@outlook.com>
2021-08-15 13:15:11 -0400

    changing the project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/yourspotify'),
+      dir: require('path').join(__dirname, './coverage/spotifyplayer'),
       subdir: '.',
       reporters: [
         { type: 'html' },
